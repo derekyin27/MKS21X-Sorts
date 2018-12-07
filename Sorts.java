04e29d394d9c901347e14f444a84b56c991d3b35
Derek <dyin00@stuy.edu>
2018-12-07 11:45:05 -0500

    class coded

--- Sorts.java
+++ Sorts.java
@@ -4,20 +4,20 @@ public class Sorts{
       int mins = ary[i];
       int index = i;
       for (int p = i; p < ary.length; p++){
-        if (ary[p] < mins ){
+        if (ary[p] < mins){
           mins = ary[p];
           index = p;
-        }
         int swap = ary[i];
         ary[index] = swap;
         ary[i] = mins;
       }
     }
+    }
   }
 
 
   public static void main(String[] args) {
-    int[] ary = {1, 9, 14, 2, 343};
+    int[] ary = {190, -97, 1423, 78, 343};
     selectionSort(ary);
     for (int i = 0; i < ary.length; i++){
       System.out.println(ary[i]);
